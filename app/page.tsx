ccd3fa2249b97853d011bc81a48b0914fb0dcb1a
osmanbastos <osmanbastos@hotmail.com>
2024-01-31 21:57:11 -0300

    add logo.png

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
-      <Image src="/logo.png" alt="logo" width="200" height="200" />
+      <Image src="/../public/logo.png" alt="logo" width="200" height="200" />
     </div>
   );
 }
